e8327ef22c90752a79de713e2d9fdcf746964843
mzfr <Mehtab.zafar98@gmail.com>
2020-06-07 21:18:46 +0530

    Visit the given directory.

--- src/main.go
+++ src/main.go
@@ -27,6 +27,17 @@ func init() {
 		fmt.Fprintf(os.Stderr, strings.Join(h, "\n"))
 	}
 }
+
+func visit(files *[]string) filepath.WalkFunc {
+	return func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			log.Fatal(err)
+		}
+		*files = append(*files, path)
+		return nil
+	}
+}
+
 // ConfigReader reads the configuration using viper
 func ConfigReader() (*viper.Viper, error) {
 	v := viper.New()
